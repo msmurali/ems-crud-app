2f468460c23c23f7f68e626dfa3d56d070458583
msmurali <muralikrishnan0511@gmail.com>
2022-06-09 15:28:02 +0530

    create components employee-list employee

---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss'],
+})
+export class EmployeeListComponent {}
