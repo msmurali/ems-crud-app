70cabd7d7dc13500a47224a173f1aae6ad43bec4
msmurali <muralikrishnan0511@gmail.com>
2022-06-10 12:37:29 +0530

    update employee-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, EmployeeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
